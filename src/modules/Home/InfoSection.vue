e4e35d28c18456ff21e0c0daf94f02752c69511f
Arbreshass <arbresha.stublla@student.uni-pr.edu>
2024-05-28 13:37:13 +0200

    info.

--- src/modules/Home/InfoSection.vue
+++ src/modules/Home/InfoSection.vue
@@ -1,41 +1,44 @@
 <template>
   <div class="infosection">
     <v-container fluid class="px-12" style="max-width: 1500px;">
+      <v-col cols="12">
+          <h3 class="news-title">Projects</h3>
+        </v-col>
       <v-row class="row">
         <v-col cols="12" md="3" class="column" :row-span="2">
           <div>
             <v-row class="inner-row">
             <v-col cols="12" class="inner-col">
-              <v-img :src="require('@/assets/1.jpg')" alt="Image 1" class="stretch-image" />
+              <v-img :src="require('@/assets/1.jpg')" alt="Image 1" class="stretch-image hover-transform" />
             </v-col>
             <v-col cols="12" class="inner-col">
-              <v-img :src="require('@/assets/2.jpg')" alt="Image 2" class="stretch-image" />
+              <v-img :src="require('@/assets/b.jpg')" alt="Image 2" class="stretch-image hover-transform" />
             </v-col>
           </v-row>
           </div>
           
         </v-col>
         <v-col cols="12" md="3" class="column">
-          <div class="im"> <v-img :src="require('@/assets/3.jpg')" alt="Image 3" class="stretch-image" /></div>
+          <div class="im"> <v-img :src="require('@/assets/c.jpg')" alt="Image 3" class="stretch-image hover-transform" /></div>
          
         </v-col>
         <v-col cols="12" md="3" class="column" :row-span="2">
           <v-row class="inner-row">
             <v-col cols="12" class="inner-col">
-              <v-img :src="require('@/assets/4.jpg')" alt="Image 4" class="stretch-image" />
+              <v-img :src="require('@/assets/feat.jpg')" alt="Image 4" class="stretch-image hover-transform" />
             </v-col>
             <v-col cols="12" class="inner-col">
-              <v-img :src="require('@/assets/5.jpg')" alt="Image 5" class="stretch-image" />
+              <v-img :src="require('@/assets/5.jpg')" alt="Image 5" class="stretch-image hover-transform" />
             </v-col>
           </v-row>
         </v-col>
         <v-col cols="12" md="3" class="column">
-          <div class="im"> <v-img :src="require('@/assets/6.jpg')" alt="Image 3" class="stretch-image" /></div>
+          <div class="im"> <v-img :src="require('@/assets/6.jpg')" alt="Image 3" class="stretch-image hover-transform" /></div>
          
         </v-col>
       </v-row>
       <v-row justify="center" align="center" class="mt-12">
-  <CostumButton buttonText="Learn More" @click="handleLearnMoreClick" />
+  <CostumButton buttonText="More Projects" @click="handleLearnMoreClick" />
 </v-row>
 
     </v-container>
@@ -47,8 +50,12 @@ import CostumButton from '@/components/CostumButton.vue';
 export default {
   components: {
     CostumButton
+  },
+  methods: {
+    handleLearnMoreClick() {
+      this.$router.push({ path: '/projects' });
+    }
   }
-
   
 };
 </script>
@@ -56,7 +63,7 @@ export default {
 <style scoped>
 .infosection {
   background-color: #131313;
-  padding-top: 200px;
+  padding-top: 150px;
 }
 
 .row {
@@ -81,7 +88,16 @@ export default {
   height: 100%;
   object-fit: cover;
   object-position: center;
-
+  transition: transform 0.3s ease-in-out;
 }
 
+.hover-transform:hover {
+  transform: scale(1.1);
+}
+
+.news-title {
+  color: #C8B560;
+  font-size: 36px;
+  margin-bottom: 40px;
+}
 </style>
